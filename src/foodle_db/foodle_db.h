965a3777141d76689d3e841380e9db454fea3fa5
Amirshokh <59666322+amig01@users.noreply.github.com>
2023-12-20 18:45:56 +0500

    Update foodle_db.h

--- src/foodle_db/foodle_db.h
+++ src/foodle_db/foodle_db.h
@@ -9,24 +9,85 @@
 #define FOODLE_DB_PASS "1234"
 #define FOODLE_DB_DATABASE "Foodle_DB"
 
-MYSQL* foodle_db_init(void);
+/*
+* @desc Function to initialize database connection
+* @return A pointer to MYSQL connection
+*/
+MYSQL*
+foodle_db_init(
+    void
+);
 
-struct foodle_user_t* foodle_db_get_user_byphone(MYSQL *con, char *phone);
+/*
+* @desc Get user by phone
+* @param[in] MYSQL *con - connection to database
+* @param[in] char *phone - phone number of user
+* @return pointer to foodle_user_t actual or user with userID = -1 if not found
+*/
+struct foodle_user_t*
+foodle_db_get_user_byphone(
+    MYSQL *con, 
+    char *phone
+);
 
-struct foodle_user_t* foodle_db_get_user_byid(MYSQL *con, int userID);
+/*
+* @desc Get user by ID
+* @param[in] MYSQL *con - connection to database
+* @param[in] int userID - ID of user
+* @return pointer to foodle_user_t actual or user with userID = -1 if not found
+*/
+struct foodle_user_t*
+foodle_db_get_user_byid(
+    MYSQL *con,
+    int userID
+);
 
-int foodle_db_add_user(MYSQL *con, struct foodle_user_t *user);
+int
+foodle_db_add_user(
+    MYSQL *con, 
+    struct foodle_user_t *user
+);
 
-int foodle_db_add_meal(MYSQL *con, struct foodle_meal_t *meal, int restaurantID);
+/*
+* @desc Function to add meal
+* @param[in] MYSQL *con - connection to database
+* @param[in] struct foodle_meal_t *meal - meal to add
+* @param[in] int restaurantID - ID of restaurant adding meal
+*/
+int
+foodle_db_add_meal(
+    MYSQL *con, 
+    struct foodle_meal_t *meal,
+    int restaurantID
+);
 
-int foodle_db_update_meal_bystruct(MYSQL *con, struct foodle_meal_t *meal);
+int
+foodle_db_update_meal_bystruct(
+    MYSQL *con,
+    struct foodle_meal_t *meal
+);
 
-int foodle_db_delete_meal_byid(MYSQL *con, int id);
+int
+foodle_db_delete_meal_byid(
+    MYSQL *con,
+    int id
+);
 
-struct foodle_meal_t* foodle_db_get_meal_byid(MYSQL *con, int id);
+struct foodle_meal_t*
+foodle_db_get_meal_byid(
+    MYSQL *con,
+    int id
+);
 
-struct foodle_restaurant_t* foodle_db_get_restaurants(MYSQL* con);
+struct foodle_restaurant_t*
+foodle_db_get_restaurants(
+    MYSQL* con
+);
 
-struct foodle_menu_t* foodle_db_get_menu(MYSQL *con, int id);
+struct foodle_menu_t*
+foodle_db_get_menu(
+    MYSQL *con,
+    int id
+);
 
-#endif // FOODLE_DB_H
+#endif //FOODLE_DB_H
